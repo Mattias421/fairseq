1541a7244159d9ffb7970944ab47a6d9be0f23bc
mattias421 <mcross2@sheffield.ac.uk>
2026-01-07 10:34:53 +0000

    integrate uv into repo

---
+++ examples/wav2vec/unsupervised/env_vars.sh
@@ -0,0 +1,3 @@
+export FAIRSEQ_ROOT=$EXP/fairseq
+export RVAD_ROOT=$EXP/rVADfast
+export KENLM_ROOT=$EXP/kenlm/build/bin
---
+++ examples/wav2vec/unsupervised/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from unsupervised!")
+
+
+if __name__ == "__main__":
+    main()
